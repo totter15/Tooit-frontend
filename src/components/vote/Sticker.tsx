4a4c42d6be8badab4073204da6a5903885680cf0
천진아 <totter15@naver.com>
2023-07-15 18:34:11 +0900

    [Fix] : 스티커 설명 좌표 수정

--- src/components/vote/Sticker.tsx
+++ src/components/vote/Sticker.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { VotedSticker } from '../../pages/Vote';
+import useResponsive from '../../hooks/useResponsive';
 
 interface StickerProps {
   sticker: VotedSticker;
@@ -14,6 +15,7 @@ function Sticker({
   stickerFocusHandler,
   stickerSize,
 }: StickerProps) {
+  const { isMobile } = useResponsive();
   const { x, y, nickname, comment, img } = sticker;
 
   const [descriptionHeight, setDescriptionHeight] = useState<
@@ -21,7 +23,7 @@ function Sticker({
   >(0); // description 높이(vh)
 
   const HEIGHT = window.innerHeight;
-  const ITEM_SIZE = 80; //  투표 아이템 사이즈(vh)
+  const ITEM_SIZE = 70; //  투표 아이템 사이즈(vh)
   const DESCRIPTION_WIDTH = 300; //  설명창 길이(px)
   const MIN_DISTANCE = 4; //  아이템 벽에서 설명창이 떨어져야할 최소한의 거리(vh)
   const STICKER_SIZE = parseInt(stickerSize, 10); // 스티커 사이즈(vh)
@@ -32,6 +34,7 @@ function Sticker({
 
   useEffect(() => {
     if (isFocused) {
+      console.log(isFocused);
       const height = document.querySelector(
         '.vote-list__item-sticker-description',
       )?.clientHeight;
@@ -43,62 +46,48 @@ function Sticker({
   const DESCRIPTION_MIN_DISTANCE =
     (DESCRIPTION_WIDTH / 2 / HEIGHT) * 100 + MIN_DISTANCE; //  설명창이 투표 아이템내부 벽에서 떨어져야할 최소 거리(vh)
 
-  //  설명창의 거리(%)가 DESCRIPTION_MIN_PERCENT보다 작을 경우 이동할 %(이동은 sticker 사이즈를 기준)
-  const DISTANCE_PERCENT_LEFT =
-    ((MIN_DISTANCE - (STICKER_X_WIDTH - STICKER_SIZE / 2)) / STICKER_SIZE) *
-    100;
-  const DISTANCE_PERCENT_RIGHT =
-    ((MIN_DISTANCE - (ITEM_SIZE - STICKER_X_WIDTH - STICKER_SIZE / 2)) /
-      STICKER_SIZE) *
-    100;
-
   const left =
     comment &&
     (STICKER_X_WIDTH < DESCRIPTION_MIN_DISTANCE
-      ? `${DISTANCE_PERCENT_LEFT}%`
-      : 'auto');
-
-  const right =
-    comment &&
-    (ITEM_SIZE - STICKER_X_WIDTH < DESCRIPTION_MIN_DISTANCE
-      ? `${DISTANCE_PERCENT_RIGHT}%`
-      : 'auto');
+      ? (DESCRIPTION_MIN_DISTANCE * HEIGHT) / 100
+      : ITEM_SIZE - STICKER_X_WIDTH < DESCRIPTION_MIN_DISTANCE
+      ? ((ITEM_SIZE - DESCRIPTION_MIN_DISTANCE) * HEIGHT) / 100
+      : x);
 
   const top =
     descriptionHeight &&
-    STICKER_SIZE * 0.7 + descriptionHeight + MIN_DISTANCE <
-      ITEM_SIZE - STICKER_Y_WIDTH
-      ? '120%'
-      : 'auto';
-
-  const bottom =
-    descriptionHeight &&
-    STICKER_SIZE * 0.7 + descriptionHeight + MIN_DISTANCE >
-      ITEM_SIZE - STICKER_Y_WIDTH
-      ? '120%'
-      : 'auto';
+    (STICKER_SIZE * 0.7 + descriptionHeight + MIN_DISTANCE <
+    ITEM_SIZE - STICKER_Y_WIDTH
+      ? (HEIGHT * (STICKER_Y_WIDTH + STICKER_SIZE * 0.7)) / 100
+      : (HEIGHT * (STICKER_Y_WIDTH - STICKER_SIZE * 0.7 - descriptionHeight)) /
+        100);
 
   return (
-    <button
-      type="button"
-      style={{
-        position: 'absolute',
-        top: y,
-        left: x,
-        width: stickerSize,
-        height: stickerSize,
-        zIndex: isFocused ? 10 : 'auto',
-      }}
-      className="vote-list__item-sticker"
-      onBlur={() => stickerFocusHandler(null)}
-      onFocus={() => stickerFocusHandler(sticker)}
-      onClick={(e) => e.stopPropagation()}
-    >
-      <img alt="sticker" src={img} />
-      {sticker.id}
+    <>
+      <button
+        type="button"
+        style={{
+          top: y,
+          left: x,
+          width: stickerSize,
+          height: stickerSize,
+          zIndex: isFocused ? 10 : 'auto',
+        }}
+        className="vote-list__item-sticker"
+        onBlur={() => stickerFocusHandler(null)}
+        onFocus={() => stickerFocusHandler(sticker)}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <img alt="sticker" src={img} />
+        {sticker.id}
+      </button>
+
       {isFocused && (
         <div
-          style={{ left, right, top, bottom }}
+          style={{
+            top: top,
+            left: left,
+          }}
           className={`vote-list__item-sticker-description ${
             !comment && 'no-comment'
           }`}
@@ -109,7 +98,7 @@ function Sticker({
           </div>
         </div>
       )}
-    </button>
+    </>
   );
 }
 
